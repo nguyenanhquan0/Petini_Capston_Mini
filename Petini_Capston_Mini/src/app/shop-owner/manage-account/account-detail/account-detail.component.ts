797b4730427ec41ae18c316b9103a6d608e42c26
nguyenanhquan0 <quan251299@gmail.com>
2023-03-11 03:33:32 +0700

    done account detail

--- Petini_Capston_Mini/src/app/shop-owner/manage-account/account-detail/account-detail.component.ts
+++ Petini_Capston_Mini/src/app/shop-owner/manage-account/account-detail/account-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-account-detail',
@@ -19,11 +20,25 @@ export class AccountDetailComponent {
   minDate!: Date;
   maxDate!: Date;
 
-  constructor(){
+  constructor(private http : UserService){
     const currentYear = new Date().getFullYear();
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() - 1);
     this.minDate = new Date(currentYear - 100, 0, 0);
     this.maxDate = new Date(currentDate);
+  }
+  ngOnInit(){
+    this.getProfile()
+  }
+  value: any;
+  getProfile(){
+    this.http.getUserInfo().subscribe((data =>{
+      console.log(data)
+      this.username = data["username"]
+      this.email = data["email"]
+      this.phone = data["phone"]
+      this.dob = data["dob"]
+      this.address = data["address"]
+    }))
   }
 }
