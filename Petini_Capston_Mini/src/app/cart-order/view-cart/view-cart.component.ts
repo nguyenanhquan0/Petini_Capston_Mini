5cfb977a145ce84ed151c45f69bb7c48d64aa8d4
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-03-10 18:56:40 +0700

    view cart

--- Petini_Capston_Mini/src/app/cart-order/view-cart/view-cart.component.ts
+++ Petini_Capston_Mini/src/app/cart-order/view-cart/view-cart.component.ts
@@ -3,31 +3,51 @@ import { ManageItemsService } from '../../services/manage-items.service';
 import { MatDialog } from '@angular/material/dialog';
 import { SuccessComponent } from '../../pop-up/success/success.component';
 import { MessageComponent } from '../../pop-up/message/message.component';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-view-cart',
   templateUrl: './view-cart.component.html',
-  styleUrls: ['./view-cart.component.scss']
+  styleUrls: ['./view-cart.component.scss'],
 })
-export class ViewCartComponent implements OnInit{
-  value:data[] = [];
-  cart:any;
-  ngOnInit(): void {
-    this.http.getCart().subscribe((data)=>{
-  this.value = data['cartProduct'];
-  console.log(data);
-  console.log(this.value);
-    },
-    (error) =>{
-      console.log(error)
-      this.message = error;
-      this.openDialogMessage();
-    })
-  }
-  message:any;
-  constructor(private http: ManageItemsService,public dialog: MatDialog,){
+export class ViewCartComponent implements OnInit {
+  value: data[] = [];
+  cart: any;
 
+  ngOnInit(): void {
+    this.http.getCart().subscribe(
+      async (data) => {
+         const cartProduct = data['cartProduct'];
+         let imgUrl ='';
+         for( let i of cartProduct){
+          await this.image
+              .getImage('items/' + i.product.imageUrl)
+              .then((url) => {
+                imgUrl = url;
+              })
+              .catch((error) => {});
+
+          this.value.push({
+            name:i.product.name,
+            description:i.product.description,
+            price:i.product.price,
+            quantity:i.quantity,
+            imageUrl:imgUrl
+          })
+         }
+
+        console.log(data);
+        console.log(this.value);
+      },
+      (error) => {
+        console.log(error);
+        this.message = error;
+        this.openDialogMessage();
+      }
+    );
   }
+  message: any;
+  constructor(private http: ManageItemsService, public dialog: MatDialog, private image: ImageService,) {}
   openDialogSuccess() {
     localStorage.setItem('registerSuccess', '');
 
@@ -49,11 +69,9 @@ export class ViewCartComponent implements OnInit{
 }
 
 export interface data {
-
   name: string;
   description: string;
-  status: string;
   imageUrl: string;
-  quantity: string;
-  price: string;
+  quantity: number;
+  price: number;
 }
